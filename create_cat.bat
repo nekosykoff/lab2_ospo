5a73f225c35c7d1a8d1ef7dacbf2ad74cbc43838
nekosykoff <167884210+nekosykoff@users.noreply.github.com>
2025-05-07 13:35:50 +0300

    last_ex

---
+++ create_cat.bat
@@ -0,0 +1,20 @@
+echo off
+md Kosykov
+cd Kosykov
+md Alex
+md Andreevich
+pause
+echo > "12 10 2003.txt"
+cd Andreevich
+echo > honor.txt
+cd ../
+cd ../
+pause
+del Kosykov /S /Q /F
+pause 
+cd Kosykov
+rd Alex 
+rd Andreevich
+cd ../
+rd Kosykov
+pause
